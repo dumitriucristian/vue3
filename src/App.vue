985147f84789ec32a64b99efee0ca47e6365fe96
cristian <dumitriucristian@yahoo.com>
2022-04-16 16:41:08 +0300

    add vue

---
+++ src/App.vue
@@ -0,0 +1,40 @@
+<template>
+  <h1>{{ title }}</h1>
+
+  <button @click="toggleModal">Toggle Modal</button>
+  <p>some text</p>
+  <div v-if="showModal">
+    <ModalVue
+      :modalTitle="modalTitle"
+      :content="modalContent"
+      :theme="theme"
+      @close="toggleModal"
+    />
+  </div>
+</template>
+
+<script>
+import ModalVue from "./components/Modal.vue";
+
+export default {
+  //showModal: true,
+  name: "App",
+  components: { ModalVue },
+  data() {
+    return {
+      title: "My First Vue app :)",
+      modalTitle: "Here is tha modal title",
+      modalContent: "This   is content of the modal",
+      theme: "sale",
+      showModal: false,
+    };
+  },
+  methods: {
+    toggleModal() {
+      this.showModal = !this.showModal;
+    },
+  },
+};
+//https://www.youtube.com/watch?v=KM1U6DqZf8M&list=PL4cUxeGkcC9hYYGbV60Vq3IXYNfDk8At1&index=5&ab_channel=TheNetNinja
+</script>
+<style></style>
---
+++ src/components/Modal.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="backdrop" @click.self="closeModal">
+    <div class="modal" :class="{ sale: theme === 'sale' }">
+      <h1>{{ modalTitle }}</h1>
+      <p>{{ content }}</p>
+    </div>
+  </div>
+</template>
+
+<script scope>
+export default {
+  props: ["modalTitle", "content", "theme", "showModal"],
+  methods: {
+    closeModal() {
+      console.log("ssss");
+      this.$emit("close");
+    },
+  },
+};
+</script>
+<style>
+.backdrop {
+  width: 100%;
+  height: 100%;
+  background-color: rgba(128, 128, 128, 0.205);
+  position: fixed;
+  top: 0;
+}
+
+.modal {
+  width: 400px;
+  height: 200px;
+  border: solid 1px;
+  background-color: white;
+  border-radius: 10px;
+  margin: 100px auto;
+}
+.sale {
+  background-color: green;
+  color: white;
+}
+</style>
